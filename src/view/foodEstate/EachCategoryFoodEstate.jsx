b3dfc0eaac2e3a6bf20aac4c91f02491b3fbb158
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2025-02-19 12:11:28 +0700

    fix: bug barchart each food estate ketika pindah ke food estate lainnya

--- src/view/foodEstate/EachCategoryFoodEstate.jsx
+++ src/view/foodEstate/EachCategoryFoodEstate.jsx
@@ -38,6 +38,16 @@ const EachCategoryFoodEstate = ({ category }) => {
     }, [responseListYear]);
 
     useEffect(() => {
+        if(category) {
+            fetchData(category, null).then((res) => {
+                console.log(res.data)
+                setDataLuasPanenToParse(formatChartData(res, 'Luas Panen (ha)'))
+                setDataProduktivitasToParse(formatChartData(res, 'Produktivitas (ku/ha)'))
+            })
+        }
+    }, [category])
+
+    useEffect(() => {
         if(category && selectedYear) {
             fetchDataAllProvinces(selectedYear, category).then((res) => {
                 console.log(res.data)
