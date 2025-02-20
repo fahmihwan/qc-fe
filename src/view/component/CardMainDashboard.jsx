0339ab26c5bcb7272a0ff4d91151bb8b44b89fab
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2025-02-20 14:13:20 +0700

    fix: slider bug

--- src/view/component/CardMainDashboard.jsx
+++ src/view/component/CardMainDashboard.jsx
@@ -11,94 +11,60 @@ const CardMainDashboard = ({ allDataFoodEstate }) => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 6,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         variableWidth: true,
+        arrows: true, 
         responsive: [
             {
-                breakpoint: 2500,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
-                }
-            },
-            {
-                breakpoint: 2000,
+                breakpoint: 1700,
                 settings: {
                     slidesToShow: 5,
                     slidesToScroll: 5,
-                }
-            },
-            {
-                breakpoint: 1900,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                }
-            },
+                    dots: true
+                    }
+                },
             {
                 breakpoint: 1500,
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 4,
-                }
-            },
+                    infinite: true,
+                    dots: true
+                    }
+                },
             {
-                breakpoint: 1400,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 1000,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 912,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                    }
+                },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                    }
+                },
             {
-                breakpoint: 390,
+                breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint:300,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
     };
+    
 
     return (
-        <div className="lg:w-[1100px] sm:w-[640px] xl:w-[1200px] box-border mx-4  ">
-            <div className="slider-container w-full overflow-hidden">
-                <Slider {...settings}>
+        <div className="lg:w-[1100px] sm:w-[480px] x xl:w-[1200px] mx-4 mb-4 box-border">
+            <div className="slider-container w-full overflow-visible">
+                <Slider {...settings} sty>
                     {dataArray.map((item, index) => (
                         <SingleCard key={index} singleData={item} />
                     ))}
@@ -110,7 +76,7 @@ const CardMainDashboard = ({ allDataFoodEstate }) => {
 
 const SingleCard = ({ singleData }) => {
     return (
-        <div className="flex border text-center dark:border-dark-mode-border bg-white bg-opacity-50 dark:bg-dark-mode dark:bg-opacity-50 justify-center rounded-lg dark:text-white w-56 h-[370px] px-4" style={{ marginRight: '10px' }}>
+        <div className="flex border text-center dark:border-dark-mode-border bg-white bg-opacity-50 dark:bg-dark-mode dark:bg-opacity-50 justify-center rounded-lg dark:text-white w-56 md:w-48 sm:w-32 h-[370px] px-4" style={{ marginRight: '10px' }}>
             <div className="w-full">
                 <h5 className="py-3 font-bold dark:text-white uppercase">{singleData.title}</h5>
                 <div className="h-[1px] dark:bg-white mb-5"></div>
