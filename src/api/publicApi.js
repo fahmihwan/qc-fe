981d858df9abab9d2a04eaf99a4ea4b8b84182c1
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2025-03-11 07:57:40 +0700

    add: add fetching data for each bencana

--- src/api/publicApi.js
+++ src/api/publicApi.js
@@ -20,3 +20,12 @@ export const getAllSummary = async(startDate, endDate, province_id=null) => {
     }
 }
 
+export const getByEachBencana = async(startDate, endDate, category_bencana, province_id=null) => {
+    try{
+        const response = await apiClient.get(`/publicapi/get-by-each-bencana?startDate=${startDate}&endDate=${endDate}${province_id ? `&id=${province_id}` : ''}&category=${category_bencana}`)
+        return response.data
+    } catch (error) {
+        console.error("error fetching data: ", error)
+        throw error
+    }
+}
